0fc1655d3f2af059a17a8ccde11f71d630ec8179
Mikhail Zinovev <mehan@list.ru>
2022-11-30 17:59:50 +0300

    1 fix

--- Task 5.py
+++ Task 5.py
@@ -10,7 +10,10 @@ def fibonacci(num: int):
         return 0
     if num in [-1, 1, 2]:
         return 1
-    return fibonacci(num - 1) + fibonacci(num - 2)
+    if num > 2:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+    if num < -1:
+        return fibonacci(num + 2) - fibonacci(num + 1)
 
 
 fib_num = int(input("Введите позицию числа Фибоначчи: "))
@@ -18,5 +21,5 @@ fib_list = list()
 i = -fib_num
 while i <= fib_num:
     fib_list.append(fibonacci(i))
-    i +=1
+    i += 1
 print(fib_list)
